0a2aa507b7288cd1fe7bd94a27c9021df6f2f341
arielp79 <apalacios79@gmail.com>
2026-03-08 17:16:00 -0300

    Modificacion en archivo route.ts

--- app/api/loans/route.ts
+++ app/api/loans/route.ts
@@ -5,8 +5,8 @@ import Loan from "@/models/Loans";
 
 // Configuramos Cloudinary con las variables que pusiste en Netlify
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+    api_key: process.env.CLOUDINARY_API_KEY!,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
